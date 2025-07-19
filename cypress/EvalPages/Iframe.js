e7aa55a72435d16bb9cf8c9ae5fd87925045b0de
rani-agrawal123 <agrawalrani2003@gmail.com>
2025-07-19 13:52:39 +0530

    new

---
+++ cypress/EvalPages/Iframe.js
@@ -0,0 +1,10 @@
+
+class Iframe{
+    iframe(){
+        cy.visit('https://www.skillsoft.com/book/web-automation');
+
+        cy.frameLoaded('.hero');
+        cy.wait(3000);
+        cy.iframe().find('#_hjSafeContext_47441005').click();
+    }
+}
